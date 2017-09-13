7f8ad119a17eb5f2b68712fec5ff2a10977f7f04
harryyang <harold.h.yang@gmail.com>
2017-09-13 18:32:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
   }
   if (string === "I love you, Grandma.") {
-    return "I love you,too."
+    return "I love you, too."
   }
 }
