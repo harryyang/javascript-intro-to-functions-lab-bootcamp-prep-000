3c08ce14db79fdbaaa7db830433f34a61c8081aa
harryyang <harold.h.yang@gmail.com>
2017-09-13 18:11:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function shout(string) {
   return string.toUpperCase()
 }
-function whisper(string){
+function whisper(string) {
   return string.toLowerCase()
 }
-function logShout(string){
+function logShout(string) {
   console.log('string'.toUpperCase)
 }
